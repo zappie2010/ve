4160e2e9f7f077aef022fad0274210367789bf26
zappie2010 <zappie2010@gmail.com>
2020-05-22 20:57:26 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        appId: 'com.samsung.sherlock.watcher',
+        nsis: {
+          shortcutName: 'watcher'
+        },
+        win: {
+          target: [
+            {
+              target: 'portable',
+              arch: [
+                'x64'
+              ]
+            }
+          ]
+        }
+      }
+    }
+  }
 }
